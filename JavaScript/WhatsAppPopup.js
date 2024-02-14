4bc767f4b55a6c175ff72c1513546eb31c5da5ea
eduardorspada <dudryspada@gmail.com>
2024-02-14 15:32:27 -0300

    Now the modal opens by itself.

--- JavaScript/WhatsAppPopup.js
+++ JavaScript/WhatsAppPopup.js
@@ -1,28 +1,3 @@
-export class WhatsAppPopup {
-    constructor(time, phone, messages, profilePicture) {
-        this.time = time;
-        this.phone = phone;
-        this.messages = messages;
-        this.profilePicture = profilePicture;
-    }
-
-    renderPopup(){
-        const popup = document.createElement('div');
-        popup.classList.add('whatsapp-popup');
-        popup.innerHTML = `
-            <div class="popup-header">
-                <div class="popup-header-left">
-                    <img src="${this.profilePicture}" alt="">
-                </div>
-                <div class="popup-header-right">
-                    <h3>${this.phone}</h3>
-                    <p>${this.time}</p>
-                </div>
-            </div>
-            <div class="popup-body">
-                ${this.messages}
-            </div>
-        `;
-        return popup;
-    }
-}
+setTimeout(() =>$(function(){
+    $('#myModal').modal('show');
+}), 45000);
